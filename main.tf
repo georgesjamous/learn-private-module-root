f167a079ad87ba181ef623787613e9966fcf0aaa
Georges Jamous <georgesaljamous@gmail.com>
2020-08-29 13:58:29 +0300

    suffix

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "s3-webapp" {
   source  = "app.terraform.io/Xeronium/s3-webapp/aws"
-  name        = var.name
+  name        = "${var.name}-edited"
   region = var.region
   prefix = var.prefix
   version = "1.0.0"
